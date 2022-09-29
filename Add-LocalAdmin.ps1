8f9d0f82f37f2fafa82ed4965079a6abb733a566
JW <jakew@smileit.com.au>
2022-09-29 14:09:14 +1000

    SET-LOCATION ADDED

--- Add-LocalAdmin.ps1
+++ Add-LocalAdmin.ps1
@@ -1,5 +1,7 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
+Set-Location $env:USERPROFILE
+
 $currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent() | Select-Object Name -ExpandProperty Name
 Write-Host "Promoting user $currentUser would you like to continue?"
 $Confirmation = Read-Host "Y/N"
